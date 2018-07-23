d9acb4a229600178b17c3a4411351440ea9d4bc1
Lam Huynh <lam.huynh@infodation.vn>
2018-07-23 11:52:59 +0700

    change loading css icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { NGXLogger  } from 'ngx-logger';
 @Component({
   // tslint:disable-next-line
   selector: 'app',
-  template: '<router-outlet></router-outlet><i class="loading  blue fa fa-spinner fa-spin fa-4x fa-fw margin-bottom" ></i>'
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
   constructor(private _logger: NGXLogger){
